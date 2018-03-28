d3c4ca9eb83c3cfc0720309ad9be96ad150e0dbe
living <707884308@qq.com>
2018-03-28 19:35:30 +0800

    chang auth

--- app/auth.py
+++ app/auth.py
@@ -5,18 +5,18 @@ OAuth2.0认证
 import sdk.auth as auth
 
 # 开发者注册信息
-CLIENT_ID = 'XXXXX'
-CLIENT_SECRET = 'XXXXXX'
+CLIENT_ID = 'AgjfSFC2fv4k8qchPrrvS46pabeSgQfR'
+CLIENT_SECRET = 'LVFsGfr78iMHoCbBs4AO6eGK55UA423i'
 
 
 
 
 def main():
     # 使用开发者注册信息
-    #auth.auth_request(CLIENT_ID, CLIENT_SECRET)
+    auth.auth_request(CLIENT_ID, CLIENT_SECRET)
 
     # 使用默认的CLIENT_ID和CLIENT_SECRET
-    auth.auth_request()
+    #auth.auth_request()
 
 
 if __name__ == '__main__':
